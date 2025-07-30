7fbfa87b47b176f4b06c43fa79ebdc8e29dd3f5f
Sakurai tomoya <onepicelove0731@gmail.com>
2025-07-30 17:18:40 +0900

    ビジネスロジックのコード修正 - OmikujiService.java

--- src/main/java/com/example/demo/service/OmikujiService.java
+++ src/main/java/com/example/demo/service/OmikujiService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
@@ -8,7 +9,24 @@ import com.example.demo.Model.OmikujiModel;
 
 @Service
 public class OmikujiService {
-    // 結果処理
-    Random random = new Random();
-    int number = random.nextInt(results.length);
+    // データ(配列)をServiceに定義
+    private final String[] results = {"大吉", "中吉", "小吉", "末吉", "凶"};
+    private final String[] luckyitems = {"スマホケース", "カメラ", "本", "CDプレーヤー", "冷蔵庫", "机", "ボールペン"};
+
+    // おみくじを引く処理
+    public OmikujiModel drawOmikuji() {
+        Random random = new Random();
+
+        // ランダムでインデックスを選択
+        int resultIndex = random.nextInt(results.length);
+        int luckyitemIndex = random.nextInt(luckyitems.length);
+
+        // 選択された結果とアイテムを取得
+        String selectedRequest = results[resultIndex];
+        String selectedLukyitem = luckyitems[luckyitemIndex];
+        LocalDate today = LocalDate.now();
+
+        // OmikujiModelを作成して返す
+        return new OmikujiModel(selectedRequest, selectedLukyitem, today);
+    }
 }
